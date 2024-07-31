183b24ed46a6f5fc3ecb2d415a93cb0cca30fad6
Shreyaaad <118002266+Shreyaaad@users.noreply.github.com>
2024-08-01 00:16:09 +0530

    Add files via upload

---
+++ Week 1/Design Pattern and Principles/ProxypatternExample/ImageViewerTest.java
@@ -0,0 +1,18 @@
+package ProxyPatternExample;
+
+public class ImageViewerTest {
+    public static void main(String[] args) {
+        Image image1 = new ProxyImage("photo1.jpg");
+        Image image2 = new ProxyImage("photo2.jpg");
+
+
+        image1.display();
+
+        image1.display();
+
+        image2.display();
+
+        image2.display();
+    }
+}
+
