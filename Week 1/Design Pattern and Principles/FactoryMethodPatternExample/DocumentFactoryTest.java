bc8ac83e90845bc4248623a3f4f1b66e46615209
Shreyaaad <118002266+Shreyaaad@users.noreply.github.com>
2024-08-01 00:08:25 +0530

    Add files via upload

---
+++ Week 1/Design Pattern and Principles/FactoryMethodPatternExample/DocumentFactoryTest.java
@@ -0,0 +1,20 @@
+package FactoryMethodPatternExample;
+
+public class DocumentFactoryTest {
+    public static void main(String[] args) {
+        
+        DocumentFactory wordFactory = new WordDocumentFactory();
+        DocumentFactory pdfFactory = new PdfDocumentFactory();
+        DocumentFactory excelFactory = new ExcelDocumentFactory();
+
+        
+        Document wordDoc = wordFactory.createDocument();
+        Document pdfDoc = pdfFactory.createDocument();
+        Document excelDoc = excelFactory.createDocument();
+
+        
+        wordDoc.open();
+        pdfDoc.open();
+        excelDoc.open();
+    }
+}
